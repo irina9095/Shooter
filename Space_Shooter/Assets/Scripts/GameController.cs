0b40083e5c0cc3ab33052cbe48f41e254b1a163f
Irina <irina9095@gmail.com>
2019-05-05 00:40:52 +0700

    New elements
    Score and main menu

---
+++ Space_Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject ScoreText;
+    public GameObject buttonStart;
+    public GameObject Menu;
+
+    public bool isGameStarted = false;
+
+    private int Score = 0;
+
+    private void Start()
+    {
+        buttonStart.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(delegate
+        {
+            buttonStart.SetActive(false);
+            Menu.SetActive(false);
+            ScoreText.SetActive(true);
+            isGameStarted = true;
+        });
+    }
+    public void IncreaseScore (int increment)
+    {
+        Score += increment;
+        ScoreText.GetComponent<UnityEngine.UI.Text>().text = "Score: "+ Score;
+    }
+}
---
+++ Space_Shooter/Assets/Sprites/AsteroidCollision.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidCollision : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Border" || other.tag == "Asteroid")
+        {
+            return;
+        }
+        if(other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+
+        }
+        else
+        {
+            GameController gameсontroller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+            gameсontroller.IncreaseScore(1);
+        }
+
+        Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    } 
+}
